0d211c298210448836d7e7684ab1a3a5cf78fcf3
Elkhan911 <955pro@mail.ru>
2022-08-14 21:51:31 +0500

    edit files of project

--- calculator.js
+++ calculator.js
@@ -1,5 +1,9 @@
 function calculator(string) {
-  // код пишите внутри этой функции
+  const symbols = string.split(" ");
+  return String(Number(symbols[0]) + Number(symbols[2]));
 }
-
-module.exports = calculator; // Не трогайте эту строчку
+// Сложение
+console.log(calculator("1 + 1"));
+console.log(calculator("1 + 2"));
+console.log(calculator("4 + 3"));
+console.log(calculator("10 + 10"));
